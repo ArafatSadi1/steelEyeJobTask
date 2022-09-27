2950b5ee34da8136a0632a2f41fbacd41d278104
ArafatSadi1 <arafatsadi1@gmail.com>
2022-09-27 19:52:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import List from "./components/List";
+import React, { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([{ text: "hello" }, { text: "world" }]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <List items={items} />
     </div>
   );
 }
